9fcc37e151ba95a9ca28e5c6671ad548e43532e9
João Marcos <jmarcos@joywork.com.br>
2022-12-13 13:50:14 -0300

    new logic

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
+let estudantes = [];
+const name = document.getElementById('name');
+const role = document.getElementById('role');
+const table = document.getElementById("divTable");
+const submit = document.getElementById('btnSubmit');
+const tbody = document.querySelector('tbody');
+
 document.getElementById('#formulario').addEventListener("submit", function (event) {
-  const name = document.getElementById('name').value;
-  const role = document.getElementById('role').value;
-  const table = document.getElementById("divTable");
-  const submit = document.getElementById('btnSubmit');
-  const tbody = document.querySelector('tbody');
 
   event.preventDefault();
+  var estudante = {
+    'name' : name.value,
+    'role' : role.value
+  }
+  estudantes.push(estudante);
 
-  var tr = document.createElement('tr');
-
-  td = document.createElement('td');
-  td.textContent = name;
-  tr.appendChild(td);
-  td = document.createElement('td');
-  td.textContent = role;
-  tr.appendChild(td);
-  console.log(name)
-
+  let tr = document.createElement('tr')
+  tr.innerHTML = `
+    <td>${name.value}</td>
+    <td>${role.value}</td>
+    <td class="acao">
+      <button><i class='bx bx-edit'></i></button>
+    </td>
+    <td class="acao">
+      <button><i class='bx bx-trash'></i></button>
+    </td>
+  `
   var tabela = document.querySelector("table tbody");
-
   tabela.appendChild(tr);
-
 });
+
+function edit(id) {
+  return false;
+}
+
+function remove(id) {
+  return false;
+}
